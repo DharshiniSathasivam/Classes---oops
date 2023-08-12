afe60059ea1708c4d88389b7e8b19e690dfc57d4
DharshiniSathasivam <dharshinisatha54@gmail.com>
2023-08-12 15:42:42 +0530

    constructor

---
+++ script.js
@@ -0,0 +1,73 @@
+//template literals
+ var a=10;
+var b=20;
+ var c=parseInt(a+b);
+ {
+ console.log(`the "value" of 
+ sum is:${c}`)
+ }
+//-------------------------------------------------------------------------
+//this method
+// const car1={
+//     model:"i20",
+//     color:"blue",
+//     drive: function()//method not a function()
+//     {
+//         console.log(`you drive this ${this.color}`)
+
+//     }
+// }
+// const car2 ={
+//     model:"x90",
+//     color:"black",
+//     drive:function(){
+//         console.log(`you drive this ${this.color}`)
+//     }
+
+// }//this is refer to globle
+// car1.drive();
+// car2.drive();
+//----------------------------------------------------------------------------
+//classes c
+// class Car{
+//     drive(){
+//         console.log("driving")
+//     }
+//     break(){
+//         console.log("brake")
+//     }
+// }
+// let car1=new Car()
+// car1.drive();
+// car1.break();
+
+// let car2=new Car()
+// car2.drive();
+//-----------------------------------------------------------
+//constructor
+
+// class Car{
+//     constructor(brand,model,color,year){
+//         this.brand=brand;
+//         this.model=model;
+//         this.color=color;
+//         this.year=year;
+//     }
+//      drive(){
+//             console.log("driving")  
+//            }
+//         break(){
+//             console.log("brake")
+//         }
+//     }
+//     let car1=new Car("hyundai","i20","red","2023")
+//     console.log(car1);
+//     ----------------------------------------------------
+
+
+
+
+
+
+
+
